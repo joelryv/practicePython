74aca3b5d3030254a6293864b308ce6e7d898e22
Joel <52022143+joelryv@users.noreply.github.com>
2022-08-16 17:07:44 -0500

    Add stack.py file
    Implementation of the stack data structure in python.

---
+++ stack.py
@@ -0,0 +1,35 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return not self.items
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[-1]
+
+    def size(self):
+        return len(self.items)
+    
+    def __str__(self):
+        return str(self.items)
+
+if __name__ == "__main__":
+    s = Stack()
+    print(s)
+    print(s.is_empty())
+    s.push(10)
+    s.push('Hola')
+    s.push(25)
+    print(s.is_empty())
+    print(s)
+    print(s.size())
+    print(s.peek())
+    s.pop()
+    print(s)
