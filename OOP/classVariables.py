0fd61863006230b74784af7ed652f59b0eba8e16
Joel <52022143+joelryv@users.noreply.github.com>
2022-08-18 19:36:14 -0500

    Update classVariables.py
    Creates a Class and two instances of it, use and modify a class variable

--- OOP/classVariables.py
+++ OOP/classVariables.py
@@ -1,1 +1,20 @@
+#!usr/bin/env python3
 
+class Empleado:
+    # numero_empleados es una variable de la clase, al ser
+    # modificada se modifica para toda la clase.
+    numero_empleados = 0
+
+    def __init__(self, nombre, apellido):
+        self.nombre = nombre
+        self.apellido = apellido
+        # Se modifica el numero de empleados que hay globalmente
+        Empleado.numero_empleados += 1
+
+if __name__ == '__main__':
+    # Aun no hay empleados
+    print(Empleado.numero_empleados)
+    emp1 = Empleado('Jose', 'Perez')
+    emp2 = Empleado('Juan', 'Lopez')
+    # Hay dos empleados
+    print(Empleado.numero_empleados)
